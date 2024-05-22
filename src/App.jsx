9827872d40855915a747f3053c0855498cce986a
Maytê <maytearaujo@yahoo.com.br>
2024-05-21 22:26:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import GeneratePDF from "./components/GeneratePDF"
+function App() {
+
+  return (
+    <>
+      <h1>Gerador de PDF</h1>
+      <GeneratePDF />
+    </>
+  );
+}
+
+export default App;
